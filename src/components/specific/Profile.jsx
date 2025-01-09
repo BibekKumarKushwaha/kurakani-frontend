eb430a9bbd220427d2a39fd6d40e3fb538b2d290
BibekKumarKushwaha <bibekkumar345@gmail.com>
2025-01-09 18:03:05 +0545

    phone number

--- src/components/specific/Profile.jsx
+++ src/components/specific/Profile.jsx
@@ -1,63 +1,3 @@
-// import React from "react";
-// import { Avatar, Stack, Typography } from "@mui/material";
-// import {
-//   Face as FaceIcon,
-//   AlternateEmail as UserNameIcon,
-//   CalendarMonth as CalendarIcon,
-// } from "@mui/icons-material";
-// import moment from "moment";
-// import { transformImage } from "../../lib/features";
-
-// const Profile = ({ user }) => {
-//   return (
-//     <Stack spacing={"2rem"} direction={"column"} alignItems={"center"}>
-//       <Avatar
-//         src={transformImage(user?.avatar?.url)}
-//         sx={{
-//           width: 200,
-//           height: 200,
-//           objectFit: "contain",
-//           marginBottom: "1rem",
-//           border: "5px solid white",
-//         }}
-//       />
-//       <ProfileCard heading={"Bio"} text={user?.bio} />
-//       <ProfileCard
-//         heading={"Username"}
-//         text={user?.username}
-//         Icon={<UserNameIcon />}
-//       />
-//       <ProfileCard heading={"Name"} text={user?.name} Icon={<FaceIcon />} />
-//       <ProfileCard
-//         heading={"Joined"}
-//         text={moment(user?.createdAt).fromNow()}
-//         Icon={<CalendarIcon />}
-//       />
-//     </Stack>
-//   );
-// };
-
-// const ProfileCard = ({ text, Icon, heading }) => (
-//   <Stack
-//     direction={"row"}
-//     alignItems={"center"}
-//     spacing={"1rem"}
-//     color={"white"}
-//     textAlign={"center"}
-//   >
-//     {Icon && Icon}
-
-//     <Stack>
-//       <Typography variant="body1">{text}</Typography>
-//       <Typography color={"gray"} variant="caption">
-//         {heading}
-//       </Typography>
-//     </Stack>
-//   </Stack>
-// );
-
-// export default Profile;
-
 
 import React from "react";
 import { Avatar, Stack, Typography, Button } from "@mui/material";
@@ -65,6 +5,7 @@ import {
   Face as FaceIcon,
   AlternateEmail as UserNameIcon,
   CalendarMonth as CalendarIcon,
+  Phone as PhoneIcon,  // Import the phone icon
 } from "@mui/icons-material";
 import moment from "moment";
 import { transformImage } from "../../lib/features";
@@ -96,6 +37,11 @@ const Profile = ({ user }) => {
         heading={"Joined"}
         text={moment(user?.createdAt).fromNow()}
         Icon={<CalendarIcon />}
+      />
+      <ProfileCard
+        heading={"Phone Number"}  // New ProfileCard for phone number
+        text={user?.phoneNumber}  // Display the phone number
+        Icon={<PhoneIcon />}  // Add phone icon
       />
 
       {/* Edit Profile Button */}
